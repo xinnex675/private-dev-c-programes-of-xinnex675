7a92fbc0c3cad4e3c79edaaa4cf33747da0e0230
xinnex675 <1467315250@qq.com>
2025-10-16 09:46:00 +0800

    Add files via upload

---
+++ if选择语句.cpp
@@ -0,0 +1,17 @@
+ #include<iostream>
+
+int main()
+{   int input = 0; 
+	printf("加入四川大学\n"); 
+    printf("要好好学习吗（0/1）？");
+    scanf("%d",&input);
+    if (input == 1)
+    {
+    	printf("好offer\n");
+	}
+	else
+	{
+		printf("卖红薯");
+	}
+    return 0;
+ } 
---
+++ while循环语句.cpp
@@ -0,0 +1,20 @@
+ #include<iostream>
+
+int main()
+{   int line = 0;
+    printf("加入四川大学\n");
+    while (line<10000)
+	{
+	    printf("写代码 ：%d\n",line);
+	    line++;
+	}    
+	if (line>=10000)
+	{
+	    printf("好offer\n")	; 
+	}
+	else
+	{
+		printf ("继续加油\n");
+	} 
+	return 0;
+ } 
